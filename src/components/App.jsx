76af4219dc948f3932c5cc636f30ae27257986f9
Oakmoater <dubrovecva@gmail.com>
2023-05-16 22:54:24 +0200

    1st task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,34 @@
-import { Component } from "react";
+import { useState } from "react";
 import Section from "./Section/Section"
 import FeedbackOptions from "./Feedback/Feedback"
 import Statistics from "./Statistics/Statistics"
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  }
+export const App = () => {
 
-  handleClickIncrement = (e) => {
+  const[good, setGood] = useState(0)
+  const[neutral, setNeutral] = useState(0)
+  const[bad, setBad] = useState(0)
+
+  // handleClickIncrement = (e) => {
+  //   const { name } = e.target;
+  //   this.setState((prevState) => ({
+  //     [name]: prevState[name] + 1,
+  //   }));
+  // };
+
+const handleClickIncrement = (e) => {
     const { name } = e.target;
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1,
-    }));
+    if (name === "good") {
+      setGood((prevState) => prevState + 1);
+    } else if (name === "neutral") {
+      setNeutral((prevState) => prevState + 1);
+    } else if (name === "bad") {
+      setBad((prevState) => prevState + 1);
+    }
   };
 
-  calculateStatistics = () => {
-    const { good, neutral, bad } = this.state;
+  const calculateStatistics = () => {
+    // const { good, neutral, bad } = this.state;
     const total = good + neutral + bad;
     const percentage = total > 0 ? Math.round((good / total) * 100) : 0;
     return {
@@ -27,16 +37,14 @@ export class App extends Component {
     };
   };
 
-  render() {
-    return (
-      <>
-        <Section title='Please leave feedback'>
-          <FeedbackOptions feedbackHandler={this.handleClickIncrement} />
-        </Section>
-        <Section title='Statistics'>
-          <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} calculator={this.calculateStatistics} />
-        </Section>
-      </>
-    )
-  };
+  return (
+    <>
+      <Section title='Please leave feedback'>
+        <FeedbackOptions feedbackHandler={handleClickIncrement} />
+      </Section>
+      <Section title='Statistics'>
+        <Statistics good={good} neutral={neutral} bad={bad} calculator={calculateStatistics} />
+      </Section>
+    </>
+  )
 };
